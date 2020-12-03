a6c6038ba0fed9ad6cf875bbfc17adeb863c53e1
Timmy73 <thomas.frion@protonmail.com>
2020-12-03 23:23:21 +0100

    API User - post

--- watsurfAPI/models/user.js
+++ watsurfAPI/models/user.js
@@ -1,12 +1,29 @@
-const mongoose = require('../db/connection'); 
+const Joi = require('joi');
+const mongoCli = require('../db/connection');
 
-const userSchema = mongoose.Schema({
-    pseudo: String,
-    email: String,
-    pwd: String,
-    score: Number
+const userSchema = Joi.object().keys({
+    pseudo: Joi.string().required(),
+    email: Joi.string().email().required(),
+    pwd: Joi.string().required(),
+    score: Joi.number().positive().min(0).default(0)
 });
 
-const User = mongoose.model('user', userSchema);
+function find(predicat, callback) {
+    const db = mongoCli.getDb();
+    db.collection('user').find(predicat).toArray(callback);
+}
 
-module.exports = User;
+function create(usr) {
+    const db = mongoCli.getDb();
+    const check = userSchema.validate(usr);
+    if(check.error || check.errors){
+        return Promise.reject(check);
+    } else {
+        return db.collection('user').insert(user);
+    }
+}
+
+module.exports = {
+    find,
+    create
+}
--- watsurfAPI/routes/user.js
+++ watsurfAPI/routes/user.js
@@ -1,16 +1,35 @@
 var express = require('express');
 var router = express.Router();
-var mongoCli = require('../db/connection');
+var user = require('../models/user');
 
-/* GET home page. */
 router.get('/users', (req, res, next) => {
-    var db = mongoCli.getDb()
-    db.collection('user').find().toArray((err, users) => {
-        if(err) {
+    user.find(undefined, (err, users) => {
+        if (err) {
             res.send(err);
         }
-        res.json(users);
+
+        res.json(users)
     });
+});
+
+router.get('/users/:pseudo([a-z]+)', (req, res) => {
+    user.find(req.params, (err, users) => {
+        if (err) {
+            res.send(err);
+        }
+
+        if(users.length == 1){
+            res.json(users[0]);
+        } else {
+            res.json(users);
+        }
+    });
+});
+
+router.post('/users', (req, res) => {
+    user.create(req.body)
+            .then(usr =>res.json(usr))
+            .catch(err => res.json(err));
 });
 
 module.exports = router;
