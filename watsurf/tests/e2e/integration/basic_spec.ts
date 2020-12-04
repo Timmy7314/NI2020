e53f26d58da669f63011c4ecf80cbc1f4c8de965
Myriam LOMBARD <thalia.discord@gmail.com>
2020-12-04 01:27:12 +0100

    Small tests with Cypress

--- watsurf/tests/e2e/integration/basic_spec.ts
+++ watsurf/tests/e2e/integration/basic_spec.ts
@@ -5,5 +5,18 @@ describe('Homepage', () => {
     cy.visit('/')
 
     cy.contains('h1', 'Hello Vue 3 + Vite + Tailwind CSS')
+
+    cy.get('button').should('be.visible')
+
+    cy.contains('button', 'count')
+    cy.contains(0)
+
+    cy.get('button').click()
+    cy.contains(1)
+
+    cy.contains('p', 'Edit components/HelloWorld.vue to test hot module replacement')
+
+    cy.get('button').click()
+    cy.contains(2)
   })
 })
