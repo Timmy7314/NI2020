312f2c323323a86e0dafd8baf6c3de5a3ac53483
Timmy73 <thomas.frion@protonmail.com>
2020-12-04 05:28:58 +0100

    Require for authorization to execute endpoints

--- watsurfAPI/src/routes/spot.js
+++ watsurfAPI/src/routes/spot.js
@@ -1,36 +1,62 @@
 const express = require('express');
 const router = express.Router();
 const spot = require('../models/spot');
+const auth = require('../services/authorization');
 
 router.get('/spots', (req, res, next) => {
-    spot.find()
-        .then(spots => res.json(spots))
-        .catch(err => res.status(400).send(err));
+    auth(req.headers.authorization)
+        .then(() => {
+            spot.find()
+                .then(spots => res.json(spots))
+                .catch(err => res.status(400).send(err));
+        })
+        .catch(err => res.status(401).json(err));
+
 });
 
 router.get('/spots/:name([a-z]+)', (req, res) => {
-    spot.find(req.params)
-        .then(spot => res.json(spot))
-        .catch(err => res.status(400).send(err));
+    auth(req.headers.authorization)
+        .then(() => {
+            spot.find(req.params)
+                .then(spot => res.json(spot))
+                .catch(err => res.status(400).send(err));
+        })
+        .catch(err => res.status(401).json(err));
+
 });
 
 router.post('/spots', (req, res) => {
-    spot.create(req.body)
-        .then(spot => res.json(spot))
-        .catch(err => res.status(400).send(err));
+    auth(req.headers.authorization)
+        .then(() => {
+            spot.create(req.body)
+                .then(spot => res.json(spot))
+                .catch(err => res.status(400).send(err));
+        })
+        .catch(err => res.status(401).json(err));
+
 });
 
 router.put('/spots/:name([a-z]+)', (req, res) => {
-    spot.update(req.body, req.params.name)
-        .then(spot => res.json(spot))
-        .catch(err => res.status(400).send(err));
+    auth(req.headers.authorization)
+        .then(() => {
+            spot.update(req.body, req.params.name)
+                .then(spot => res.json(spot))
+                .catch(err => res.status(400).send(err));
+        })
+        .catch(err => res.status(401).json(err));
+
 });
 
 router.delete('/spots/:name([a-z]+)', (req, res) => {
-    spot.remove(req.params.name)
-        .then(spot => res.json(spot))
-        .catch(err => res.status(400).send(err));
-}); 
+    auth(req.headers.authorization)
+        .then(() => {
+            spot.remove(req.params.name)
+                .then(spot => res.json(spot))
+                .catch(err => res.status(400).send(err));
+        })
+        .catch(err => res.status(401).json(err));
+
+});
 
 
 
