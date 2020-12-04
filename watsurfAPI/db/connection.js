7f586a3a06025759f65e410a17aa21b36cc662cf
Timmy73 <thomas.frion@protonmail.com>
2020-12-04 02:26:51 +0100

    Change database name

--- watsurfAPI/db/connection.js
+++ watsurfAPI/db/connection.js
@@ -1,8 +1,3 @@
-// var mongoose = require('mongoose');
-// mongoose.connect('mongodb://watsurf:watsurf@127.0.0.1:27017/watsurf');
-
-// module.exports = mongoose;
-
 const MongoClient = require('mongodb').MongoClient;
 const url = 'mongodb://watsurf:watsurf@127.0.0.1:27017';
 
@@ -11,7 +6,7 @@ var _db;
 module.exports = {
     connect: (callback) => {
         MongoClient.connect(url, {useNewUrlParser: true}, (err, cli) => {
-            _db = cli.db('watsurf');
+            _db = cli.db('watsurfer');
             return callback(err);
         });
     },
